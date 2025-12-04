ea721f6e174362295f811d77a4f9d36798285e6a
Carl von Ayres <cvonayres@fuelcellsystems.co.uk>
2025-12-04 15:24:26 +0000

    Day 3 Task 2

--- advent/day03.py
+++ advent/day03.py
@@ -1,9 +1,11 @@
 from advent.utils import parse_common_args, load_lines
 
+# Input loading function
 def load_input(path: str, debug: bool = False) -> list[str]:
     return load_lines(path, debug=debug)
 
-def run_logic(data: list[str], task: int, debug: bool = False) -> int:
+# Main logic function
+def run_logic(data: list[str], task: int, debug: bool = False, bucket_size: int = 12) -> int:
     result = 0
     
     for entry in data:
@@ -12,9 +14,10 @@ def run_logic(data: list[str], task: int, debug: bool = False) -> int:
             case 1:
                 result += max_pair_value(entry)
             case 2:
-                raise NotImplementedError("Task 2 not implemented yet")
+                result += max_number_from_line(entry, bucket_size)
     return result
 
+# Task 1 helper
 def max_pair_value(line: str) -> int:
     max_value = -1
     length = len(line)
@@ -25,6 +28,32 @@ def max_pair_value(line: str) -> int:
                 max_value = pair_value
     return max_value
          
+# Task 2 helper
+def max_number_from_line(line: str, digits_to_pick: int) -> int:
+    length = len(line)
+    assert digits_to_pick <= length, "Cannot pick more digits than available"
+
+    start_index = 0
+    chosen_digits: list[str] = []
+
+    while len(chosen_digits) < digits_to_pick:
+        # pick first digit
+        remaining_to_pick = digits_to_pick - len(chosen_digits)
+        end_index = length - remaining_to_pick
+        best_digit = "-1"
+        best_index = start_index
+
+        for index in range(start_index, end_index + 1):
+            digit = line[index]
+            if digit > best_digit:
+                best_digit = digit
+                best_index = index
+
+        chosen_digits.append(best_digit)
+        start_index = best_index + 1
+    
+    return int("".join(chosen_digits))
+    
 def main():
     # Parse command-line arguments
     args = parse_common_args()
